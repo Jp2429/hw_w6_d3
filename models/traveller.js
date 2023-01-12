8e573a8c0634bc20cc4c0776336fa0af66c45719
John Panton <johnpanton29@outlook.com>
2023-01-12 09:49:06 +0000

    made small changes

--- models/traveller.js
+++ models/traveller.js
@@ -3,21 +3,32 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-    const journeyArray = []
-    this.journeys.forEach((journey) =>  {
-      journeyArray.push(journey.startLocation)
+    // const journeyArray = []
+    // this.journeys.forEach((journey) =>  {
+    //   journeyArray.push(journey.startLocation)
   
+    // })
+    // return journeyArray
+
+
+    //Better way of doing it
+    return this.journeys.map((journey)=>{
+      return journey.startLocation
     })
-    return journeyArray
   };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  const endArray = []
-    this.journeys.forEach((journey) =>  {
-      endArray.push(journey.endLocation)
+  // const endArray = []
+  //   this.journeys.forEach((journey) =>  {
+  //     endArray.push(journey.endLocation)
   
-    })
-    return endArray
+  //   })
+  //   return endArray
+
+  // Bettter way of doing it
+  return this.journeys.map((journey)=>{
+    return journey.endLocation
+  })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
@@ -35,26 +46,37 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-  let journeyDistanceArray = []
-  const totalDistance = this.journeys.forEach((journey) => {
-    journeyDistanceArray.push(journey.distance)
-  })
-  const total = journeyDistanceArray.reduce((runningTotal, currentValue) => {
-    return runningTotal + currentValue
-  }, 0)
+  // let journeyDistanceArray = []
+  // const totalDistance = this.journeys.forEach((journey) => {
+  //   journeyDistanceArray.push(journey.distance)
+  // })
+  // const total = journeyDistanceArray.reduce((runningTotal, currentValue) => {
+  //   return runningTotal + currentValue
+  // }, 0)
+
+
+  //Better way of doing it
+  const total=this.journeys.reduce((runningTotal,journey)=>{
+    return runningTotal+journey.distance
+  },0)
   return total
 };
 
+//Pleased with this
+
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  let jArray=[]
+  // let jArray=[]
   function onlyUnique(value, index, self) { 
     return self.indexOf(value) === index;
 }
-  const uniqueTransport=this.journeys.forEach((journey)=>{
-    jArray.push(journey.transport)
+  // const uniqueTransport=this.journeys.forEach((journey)=>{
+  //   jArray.push(journey.transport)
+  // })
+  const uniqueTransport=this.journeys.map((journey)=>{
+    return journey.transport
   })
   
-  var unique = jArray.filter( onlyUnique );
+  var unique = uniqueTransport.filter( onlyUnique );
 
   return unique;
 };
